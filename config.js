20436a860bc1866a5d712addd40f81c012faf5d4
pirenna <91009235+pirenna@users.noreply.github.com>
2021-09-19 17:38:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,15 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Âyyyyyy',
+    introDesc: 'Ở đây có quà trung thu nè :)))`,
+    btnIntro: 'Ấn vào đây', 
+    btnYes: 'Nhận',
+    btnNo: 'Hên xui',
+    question: 'Có thực sự muốn nhận ko đấy...Yes or No',
+    btnReply: 'Gửi',
+    reply: 'Yes',
+    mess: 'Quà của anh là Em đây :3',
+    messDesc: 'Đúng không nhầm đâu It is meeeee',
+    btnAccept: 'Okela',
+    messLink: 'https://www.messenger.com/t/dieungoc.03' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
